7dc73a7e1d705d339d0b3ec29c64118816c05699
MeowServer~ <2679977872@qq.com>
2026-03-04 10:09:06 -0500

    Improve HintServiceExample

--- HintServiceExample/Plugin.cs
+++ HintServiceExample/Plugin.cs
@@ -7,6 +7,7 @@
     using HintServiceMeow.Core.Utilities;
     using HintServiceMeow.UI.Utilities;
     using LabApi.Events.Arguments.PlayerEvents;
+    using LabApi.Features.Wrappers;
     using UnityEngine;
     using Hint = HintServiceMeow.Core.Models.Hints.Hint;
 
@@ -44,35 +45,52 @@
         public static void OnVerified(PlayerJoinedEventArgs ev)
         {
             // Hint is a simple hint that can be shown on the player's screen.
-            Hint hint = new Hint
+            Hint hint1 = new Hint
             {
                 Text = "Hello World" // You can set the properties of the hint in a pair of braces({})
             };
 
+            Hint hint2 = new Hint
+            {
+                AutoText = ev => DateTime.Now.ToString("HH:mm:ss"), // You can also use a function to set the text of the hint, and the hint will update itself.
+                Alignment = HintAlignment.Left, // You can set the properties of the hint in any order you want, and you can also choose to not set some properties, because all properties have a default value.
+                YCoordinate = 200
+            };
+
+            Hint hint3 = new Hint()
+            {
+                AutoText = ev =>
+                {
+                    ev.NextUpdateDelay = TimeSpan.FromSeconds(5f); // You can set the next update delay in the event argument, and the hint will update itself after the delay. This is useful when you want to update the hint after a certain time interval.
+
+                    return Server.Tps.ToString("F2");
+                }
+            };
+
             // You can set the properties of the hint like this:
-            hint.FontSize = 40;
-            hint.YCoordinate = 700;
-            hint.Alignment = HintAlignment.Left;
+            hint1.FontSize = 40;
+            hint1.YCoordinate = 700;
+            hint1.Alignment = HintAlignment.Left;
             // After setting the properties, you don't need to use method to call for an update. All update will be done automatically by the HSM(HintServiceMeow).
 
             // You can show a hint to a player by adding it to the player's PlayerDisplay.
             // You can also remove it by removing it from the PlayerDisplay.
             PlayerDisplay playerDisplay = PlayerDisplay.Get(ev.Player);
-            playerDisplay.AddHint(hint);
-            playerDisplay.RemoveHint(hint);
+            playerDisplay.AddHint(hint1);
+            // playerDisplay.RemoveHint(hint);
 
             // You can also use extension method to make it easier
-            ev.Player.AddHint(hint); // This is equivalent to playerDisplay.AddHint(hint);
-            ev.Player.RemoveHint(hint); // This is equivalent to playerDisplay.RemoveHint(hint);
+            ev.Player.AddHint(hint2); // This is equivalent to playerDisplay.AddHint(hint);
+            // ev.Player.RemoveHint(hint); // This is equivalent to playerDisplay.RemoveHint(hint);
 
             // If you only want to show a hint temporarily, you can use ShowHint
-            playerDisplay.ShowHint(hint, 12f); // This show a hint for 12 second, then hide it.
+            playerDisplay.ShowHint(hint3, 12f); // This show a hint for 12 second, then hide it.
 
             // DynamicHint is a hint that can automatically arrange itself to avoid overlapping with other hints.
             DynamicHint dynamicHint = new DynamicHint
             {
                 Text = "Hello Dynamic Hint",
-                TargetX = 100f, // You can choose to set a property or not to in a braces. All properties have a default value, so you can just set the properties you want to change.
+                TargetX = 100f,
             };
 
             playerDisplay.AddHint(dynamicHint);
