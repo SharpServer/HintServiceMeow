ce7e3e36d9e163e13144f2b0bc3d97cac151d980
MeowServer~ <2679977872@qq.com>
2026-03-05 23:55:56 -0500

    Organize HSM.Benchmark's structure

---
+++ HintServiceMeow.Benchmarks/TestLogger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using HintServiceMeow.Core.Interface;
+
+namespace HintServiceMeow.Benchmarks
+{
+    class TestLogger : ILogger
+    {
+        public void Error(object ex)
+        {
+            Console.WriteLine($"[TestLogger][Error] {ex}");
+        }
+
+        public void Info(object message)
+        {
+            Console.WriteLine($"[TestLogger][Info] {message}");
+        }
+
+        public void Debug(object message)
+        {
+            Console.WriteLine($"[TestLogger][Debug] {message}");
+        }
+    }
+}
